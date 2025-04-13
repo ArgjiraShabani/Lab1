5a2e24008dd357d1befb31eca71060ff12ba0863
ArgjiraShabani <argjirashabani@gmail.com>
2025-04-13 15:57:17 +0200

    Add routing functionality with react-router-dom, removing duplicate package.json file

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,18 +1,33 @@
 import Navbar from './Navbar';
 import Footer from './Footer';
+import HomePage from './Pages/HomePage';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+import React from 'react';
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
 
 
 function App() {
   return (
     <>
-    <div className="d-flex flex-column min-vh-100">
-    <Navbar/>
-    <main className="flex-grow-1"></main>
-    <Footer/>
-    </div>
+    
+    
+      <Router>
+        <div className="d-flex flex-column min-vh-100">
+          <Navbar/>
+            <main className="flex-grow-1">
+              <Routes>
+                <Route path='/' element={<HomePage/>}/>
+
+              </Routes>
+            </main>
+          <Footer/>
+        </div>
+      </Router>
+
+    
+    
     </>
 
   );
