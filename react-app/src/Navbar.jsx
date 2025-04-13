6cf474066dcfcb9e90ce21010fd175772a3f2346
ArgjiraShabani <argjirashabani@gmail.com>
2025-04-13 15:58:49 +0200

    Add routing functionality with react-router-dom, removing duplicate package.json file

--- react-app/src/Navbar.jsx
+++ react-app/src/Navbar.jsx
@@ -4,9 +4,10 @@ function Navbar(){
   return(
     <>
     <nav className="navbar navbar-expand-lg " >
+     
     
   <div className="container-fluid">
-    <a className="navbar-brand" href="#" style={{ fontSize: '30px' }}>CareWave </a>
+    <a className="navbar-brand" href="/Pages/HomePage'" style={{ fontSize: '30px' }}>CareWave </a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon" ></span>
     </button>
